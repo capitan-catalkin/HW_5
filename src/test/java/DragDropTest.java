f3613dd56bb98e081bb8b5d2b3bd93e507aad613
urrrrk <pomogitemnenaiti@mail.ru>
2023-09-19 17:26:53 +0300

    DragDropTest

---
+++ src/test/java/DragDropTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragDropTest {
+    @Test
+    void dragDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        Configuration.pageLoadStrategy = "eager";
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+
+    }
+}
